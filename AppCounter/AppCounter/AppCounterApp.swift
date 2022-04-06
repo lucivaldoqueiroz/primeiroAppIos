7dc8d72ac4e707622e948e96df22ae1e4e9c3f86
lucivaldoqueiroz <lucivaldo.npqueiroz@gmail.com>
2022-04-05 23:07:41 -0300

    App contador

---
+++ AppCounter/AppCounter/AppCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCounterApp.swift
+//  AppCounter
+//
+//  Created by Lucivaldo Queiroz on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct AppCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppCounter/AppCounter/ContentView.swift
@@ -0,0 +1,84 @@
+//
+//  ContentView.swift
+//  AppCounter
+//
+//  Created by Lucivaldo Queiroz on 05/04/22.
+//
+
+import SwiftUI
+
+class Counter: ObservableObject {
+    
+    @Published var days = 0
+    @Published var hours = 0
+    @Published var minutes = 0
+    @Published var seconds = 0
+    
+    var selectedDate = Date()
+    
+    init() {
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            let calendar = Calendar.current
+            
+            let components = calendar.dateComponents([.year, .day, .month, .hour, .minute, .second], from: Date())
+            
+            let currentDate = calendar.date(from: components)
+            
+            let selectedComponents = calendar.dateComponents([.year, .day, .month, .hour, .minute, .second], from: self.selectedDate)
+            
+            var eventDateComponents = DateComponents()
+            eventDateComponents.year = selectedComponents.year
+            eventDateComponents.month = selectedComponents.month
+            eventDateComponents.day = selectedComponents.day
+            eventDateComponents.hour = selectedComponents.hour
+            eventDateComponents.minute = selectedComponents.minute
+            eventDateComponents.second = selectedComponents.second
+            
+            let eventDate = calendar.date(from: eventDateComponents)
+            
+            
+            let timeLeft = calendar.dateComponents([.day, .hour, .minute, .second], from: currentDate!, to: eventDate!)
+            
+            if(timeLeft.second! >= 0){
+                self.days = timeLeft.day ?? 0
+                self.hours = timeLeft.hour ?? 0
+                self.minutes = timeLeft.minute ?? 0
+                self.seconds = timeLeft.second ?? 0
+            }
+        }
+    }
+}
+
+
+struct ContentView: View {
+    
+   @StateObject var counter = Counter()
+    
+    var body: some View {
+        VStack {
+            
+            DatePicker(selection: $counter.selectedDate, in: Date()..., displayedComponents: [.hourAndMinute, .date]) {
+                Text("Selecione a data: ")
+            }
+            
+            
+            HStack{
+                Text("\(counter.days) dias")
+                    .padding()
+                    .background(Color.blue)
+                
+                Text("\(counter.hours) horas")
+                Text("\(counter.minutes) min")
+                Text("\(counter.seconds) seg")
+            }
+
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
